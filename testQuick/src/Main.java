181d0741a262392c33124352ce0d50e38f65ef5a
Victor Rodrigues <victortavi@outlook.com>
2023-03-14 12:49:13 -0300

    Teste adicionado
    Validação do método quickSortSemPivoRecursivo foi adicionado

--- testQuick/src/Main.java
+++ testQuick/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void quickSortSemPivoRecursivo(int inicio, int fim, int[] vet)
@@ -31,16 +34,52 @@ public class Main {
     public static void quickSortSemPivo(int[] vet) {
         quickSortSemPivoRecursivo(0, vet.length - 1, vet);
     }
-    public static void main(String[] args) {
-        int []arr = {4, 2, 1, 6, 7, 12, 9, 11};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("[%d]", arr[i]);
-        }
-        quickSortSemPivo(arr);
-        System.out.println("\n------------------");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("[%d]", arr[i]);
+
+    public static void display(int... arr)
+    {
+        for (int j : arr) {
+            System.out.printf("[%d]", j);
         }
         System.out.println();
+    }
+    static final Random random = new Random();
+    public static int getRandNumber(int min, int max)
+    {
+        return random.nextInt(max - min + 1) + min;
+    }
+    
+    public static boolean validaOrdenacao(int[] arr)
+    {
+        // Array has one or no element
+        if (arr.length == 0 || arr.length == 1)
+            return true;
+
+        for (int i = 1; i < arr.length; i++)
+
+            // Unsorted pair found
+            if (arr[i - 1] > arr[i])
+                return false;
+
+        // No unsorted pair found
+        return true;
+    }
+    public static void main(String... args) {
+
+        int[] arr = new int[getRandNumber(50, 100)];
+        System.out.printf("[%d] Elementos no array\n", arr.length);
+
+        Arrays.setAll(arr, i -> getRandNumber(0, 100));
+
+        display(arr);
+        System.out.println("------------------");
+
+        quickSortSemPivo(arr);
+
+        display(arr);
+
+        if(validaOrdenacao(arr))
+            System.out.println("Vetor está ordenado!");
+        else
+            System.out.println("Vetor desordenado!");
     }
 }
